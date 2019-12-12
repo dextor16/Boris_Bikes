f65ba034448b28b3479816e2f9c1ab6f39032b5e
dextor16 <jamestcork92@gmail.com>
2019-12-12 15:11:48 +0000

    third commit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+
+    attr_reader :bike
     
     def release_bike
-        Bike.new
+        # Bike.new
+        @bike
     end
-    
-    def dock_bike
+
+    def dock_bike(bike)
+        @bike = bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,18 +1,32 @@
 require 'docking_station.rb'
 
 describe DockingStation do 
-    it 'responds to the method release bike' do
-        expect(DockingStation.new).to respond_to(:release_bike)
+    describe'#release_bike' do
+    it 'Releases a bike' do
+        bike = Bike.new
+        station = DockingStation.new
+        station.dock_bike(bike)
+        expect(station.release_bike).to eq bike
+        end
     end
+
+    # it 'responds to the method release bike' do
+    # expect(DockingStation.new).to respond_to(:release_bike)
+    # end     
 
     it "Should release a working bike" do
-        expect(DockingStation.new.release_bike.working?).to eq true
+        bike = Bike.new
+        station = DockingStation.new
+        station.dock_bike(bike)
+        expect(station.release_bike.working?).to eq true
     end
 
-    it 'responds to the method dock bike' do
-        expect(DockingStation.new).to respond_to(:dock_bike)
+    it "responds to the method dock bike" do
+        expect(DockingStation.new).to respond_to(:dock_bike).with(1).argument
     end
 
-        
+    it "Should fail if no bikes are docked" do
+        expect(DockingStation.new.release_bike).to raise_error("There are no bikes")
+    end
+end 
 
-end
